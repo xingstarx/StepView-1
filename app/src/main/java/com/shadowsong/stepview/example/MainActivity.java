80ec8c60aa9e7f76f696c0c34d2ca67092a3b44d
xingxing <xxx823952375@gmail.com>
2017-04-20 23:10:16 +0800

    add demo

--- app/src/main/java/com/shadowsong/stepview/example/MainActivity.java
+++ app/src/main/java/com/shadowsong/stepview/example/MainActivity.java
@@ -13,62 +13,67 @@ import com.shadowsong.stepview.VerticalStepDecoration;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    initList();
-  }
-
-  private void initList() {
-    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-    // recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,
-    // LinearLayoutManager.VERTICAL));
-    recyclerView
-        .setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-
-    VerticalStepDecoration stepDecoration = new VerticalStepDecoration.Builder(this)
-        .currentRes(R.drawable.step_current)
-        .doneRes(R.drawable.step_done)
-        .undoneRes(R.drawable.step_undone)
-        .width(200)
-        .lineWidth(3)
-        .lineColor(0xff11cd6e)
-        .dashColor(0xff11cd6e)
-        .currentStep(25)
-        .build();
-
-
-    recyclerView.addItemDecoration(stepDecoration);
-    recyclerView.setAdapter(new ItemAdapter());
-
-  }
-
-  class ItemHolder extends RecyclerView.ViewHolder {
-
-    TextView contentTextView;
-
-    public ItemHolder(View itemView) {
-      super(itemView);
-      contentTextView = (TextView) itemView.findViewById(R.id.content_tv);
-    }
-  }
-  class ItemAdapter extends RecyclerView.Adapter<ItemHolder> {
-
     @Override
-    public ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-      return new ItemHolder(
-          LayoutInflater.from(parent.getContext()).inflate(R.layout.cell, parent, false));
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initList();
     }
 
-    @Override
-    public void onBindViewHolder(final ItemHolder holder, int position) {
-      holder.contentTextView.setText("Step " + position);
+    private void initList() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        // recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,
+        // LinearLayoutManager.VERTICAL));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false) {
+                    @Override
+                    public boolean canScrollVertically() {
+                        return false;
+                    }
+                });
+
+        VerticalStepDecoration stepDecoration = new VerticalStepDecoration.Builder(this)
+                .currentRes(R.drawable.step_current)
+                .doneRes(R.drawable.step_done)
+                .undoneRes(R.drawable.step_undone)
+                .width(200)
+                .lineWidth(3)
+                .lineColor(0xff11cd6e)
+                .dashColor(0x5a5a5a5a)
+                .currentStep(0)
+                .build();
+
+
+        recyclerView.addItemDecoration(stepDecoration);
+        recyclerView.setAdapter(new ItemAdapter());
+
     }
 
-    @Override
-    public int getItemCount() {
-      return 50;
+    class ItemHolder extends RecyclerView.ViewHolder {
+
+        TextView contentTextView;
+
+        public ItemHolder(View itemView) {
+            super(itemView);
+            contentTextView = (TextView) itemView.findViewById(R.id.content_tv);
+        }
     }
-  }
+
+    class ItemAdapter extends RecyclerView.Adapter<ItemHolder> {
+
+        @Override
+        public ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ItemHolder(
+                    LayoutInflater.from(parent.getContext()).inflate(R.layout.cell, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(final ItemHolder holder, int position) {
+            holder.contentTextView.setText("Step " + position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
+    }
 }
